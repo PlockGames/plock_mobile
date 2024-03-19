78813645339cdce687a17291b50eccb5d12e252c
pebloop <pebloop@outlook.com>
2024-03-19 13:57:04 +0100

    feat: Add delete button for my games page

--- test/my_games_page_test.dart
+++ test/my_games_page_test.dart
@@ -70,5 +70,40 @@ void main() {
     expect(find.text('Test'), findsOneWidget);
   });
 
+  testWidgets('Delete a game test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify page content
+    expect(find.text('Mes projets'), findsOneWidget);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Modify the textfield
+    await tester.enterText(find.byType(TextField), 'Test');
+    await tester.pump();
+
+    // Tap the 'Créer' button and trigger a frame.
+    await tester.tap(find.text('Créer'));
+    await tester.pump();
+
+    // Verify that the game is created and that the game edition page is displayed
+    expect(find.text('Test'), findsOneWidget);
+
+    // go back to the games page
+    final dynamic widgetsAppState = tester.state(find.byType(WidgetsApp));
+    await widgetsAppState.didPopRoute();
+    await tester.pump();
+
+    // Tap the 'delete' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.delete));
+    await tester.pump();
+
+    // Verify that the game is deleted
+    expect(find.text('Test'), findsNothing);
+  });
+
 
 }
