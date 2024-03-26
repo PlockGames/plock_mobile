38276e21ca7851355d392074a363a3b1cebf30c3
pebloop <pebloop@outlook.com>
2024-03-26 10:24:27 +0100

    feat: add add components in object

--- lib/models/games/game_object.dart
+++ lib/models/games/game_object.dart
@@ -2,7 +2,7 @@ import 'package:plock_mobile/models/utils/Vector2.dart';
 import 'component.dart';
 
 class GameObject {
-  final String name;
+  late final String name;
   List<Component> components = List<Component>.empty(growable: true);
   Vector2 position = Vector2(0, 0);
 
