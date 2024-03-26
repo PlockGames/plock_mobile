88a2e7acdb8a4e22c133c763a463848a21321315
pebloop <pebloop@outlook.com>
2024-03-26 12:33:52 +0100

    test: Add component edition tests

--- lib/models/games/game_object.dart
+++ lib/models/games/game_object.dart
@@ -2,7 +2,7 @@ import 'package:plock_mobile/models/utils/Vector2.dart';
 import 'component_type.dart';
 
 class GameObject {
-  late final String name;
+  late String name;
   List<ComponentType> components = List<ComponentType>.empty(growable: true);
   Vector2 position = Vector2(0, 0);
 
