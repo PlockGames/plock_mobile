9aff1388d4b12a2b712ea9438b16db72cc4f82b2
pebloop <pebloop@outlook.com>
2024-03-26 11:46:22 +0100

    feat: component edition

---
+++ lib/models/games/component_field.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ComponentField {
+
+  String get type => 'Field';
+
+  Widget getField(String name) {
+    return Container();
+  }
+
+  ComponentField instance() {
+    return ComponentField();
+  }
+}
--- lib/models/games/game_object.dart
+++ lib/models/games/game_object.dart
@@ -1,9 +1,9 @@
 import 'package:plock_mobile/models/utils/Vector2.dart';
-import 'component.dart';
+import 'component_type.dart';
 
 class GameObject {
   late final String name;
-  List<Component> components = List<Component>.empty(growable: true);
+  List<ComponentType> components = List<ComponentType>.empty(growable: true);
   Vector2 position = Vector2(0, 0);
 
   GameObject({required this.name});
