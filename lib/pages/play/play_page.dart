8f4ffa3f933a2edd43cbef64eb54760628b72eb5
pebloop <pebloop@outlook.com>
2024-06-17 06:10:16 +0200

    feat: Add navbar

---
+++ lib/pages/play/play_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PlayPage extends StatelessWidget {
+  const PlayPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Play Page'),
+    );
+  }
+}
